e9a3ddd29bf3d797e25aca175584b88cbc158b38
langletamelie <amelie.langlet@hotmail.com>
2018-06-25 15:03:19 +0200

    exo3

---
+++ assets/js/js3exo3.js
@@ -0,0 +1,12 @@
+//déclaration de la fonction pour changer le texte en vert
+function changeColorG(){
+document.getElementById('text').style.color='green';
+}
+//déclaration de la fonction pour changer le texte en rouge
+function changeColorR(){
+document.getElementById('text').style.color='red';
+}
+//déclaration de la fonction pour changer le texte en bleu
+function changeColorB(){
+document.getElementById('text').style.color='blue';
+}
